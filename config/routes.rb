eec75ea0ab16fd0842a1ed619d6c2d1f725942e8
Jim Hessin <jhessin@gmail.com>
2019-12-29 17:53:00 -0700

    add test for root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'foo/bar'
   get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
 end
